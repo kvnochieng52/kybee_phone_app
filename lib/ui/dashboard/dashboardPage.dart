274bf810845e5f7b2fa58a1258ae6c51ad7ee6c3
Kevin Ochieng <7-kochieng@users.noreply.github.wananchi.com>
2022-02-09 15:49:44 +0300

    Apply loan started

--- lib/ui/dashboard/dashboardPage.dart
+++ lib/ui/dashboard/dashboardPage.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:kybee/api/api.dart';
 import 'package:kybee/common/theme_helper.dart';
+import 'package:kybee/ui/loading.dart';
 import 'package:kybee/ui/loan/LoanApprovedPage.dart';
 import 'package:kybee/widgets/drawer.dart';
 import 'package:kybee/widgets/headerMain.dart';
@@ -20,14 +21,64 @@ class DashboardPage extends StatefulWidget {
 class _DashboardState extends State<DashboardPage> {
   List _loanDistributions = [];
   int _loanDistribution = 0;
-  String _currency;
+  String _currency = '-';
   int _loanpaymentDays = 0;
+
+  String _totalLoanAmount = "-";
+  String _intrest = "-";
+  String _commission = "-";
+  String _loanDisbursed = "-";
+  String _applicationDateFormatted = "-";
+  String _dueDateFormatted = "-";
 
   bool _initDataFetched = false;
 
   void initState() {
     super.initState();
     _getInitData();
+  }
+
+  _calculateLoan(context) async {
+    Loading().loader(context, "Loading...Please wait");
+
+    var data = {
+      'distribution_id': _loanDistribution,
+    };
+    var res = await CallApi().postData(data, 'loan/calculate_loan');
+    var body = json.decode(res.body);
+
+    if (body['success']) {
+      setState(() {
+        _loanpaymentDays = body['loan_details']['loan_details']['period'];
+        _totalLoanAmount = body['loan_details']['total_loan_amount_formatted'];
+        _intrest = body['loan_details']['intrest_formatted'];
+        _commission = body['loan_details']['commission_formatted'];
+        _loanDisbursed = body['loan_details']['disbursed_formatted'];
+        _applicationDateFormatted =
+            body['loan_details']['application_date_formatted'];
+        _dueDateFormatted = body['loan_details']['due_date_formatted'];
+      });
+    }
+
+    Navigator.pop(context);
+  }
+
+  _applyLoan(context) async {
+    Loading().loader(context, "Loading...Please wait");
+
+    SharedPreferences localStorage = await SharedPreferences.getInstance();
+    var user = json.decode(localStorage.getString('user'));
+
+    var data = {
+      'user_id': user['id'],
+      'loan_distribution_id': _loanDistribution,
+    };
+    var res = await CallApi().postData(data, 'loan/apply_loan');
+    var body = json.decode(res.body);
+
+    //if (body['success']) {}
+
+    Navigator.pop(context);
   }
 
   _getInitData() async {
@@ -46,20 +97,29 @@ class _DashboardState extends State<DashboardPage> {
         _loanDistribution = body['user_details']['loan_distribution_id'];
         _loanpaymentDays = body['user_details']['period'];
         _currency = body['currency'];
+        _totalLoanAmount = body['default_loan']['total_loan_amount_formatted'];
+        _intrest = body['default_loan']['intrest_formatted'];
+        _commission = body['default_loan']['commission_formatted'];
+        _loanDisbursed = body['default_loan']['disbursed_formatted'];
+        _dueDateFormatted = body['default_loan']['due_date_formatted'];
+        _applicationDateFormatted =
+            body['default_loan']['application_date_formatted'];
         _initDataFetched = true;
       });
     }
   }
 
-  _format_currency(double d) {
+  _formatCurrency(double d) {
     String inString = d.toStringAsFixed(0);
     var formatter = NumberFormat('#,##,000');
     return formatter.format(double.tryParse(inString));
   }
 
-  _refreshScreen() {
-    print('refreshed');
-    // _format_currency(1000.00);
+  _refreshScreen(context) {
+    _initDataFetched = false;
+    Loading().loader(context, "Loading...Please wait");
+    _getInitData();
+    Navigator.pop(context);
   }
 
   @override
@@ -84,67 +144,48 @@ class _DashboardState extends State<DashboardPage> {
   Container _loanSelecetor(context) {
     return Container(
       child: Center(
-        child: DropdownButton(
-          value: _loanDistribution,
-          isExpanded: false,
-          hint: Text("Select Loan"),
-          // validator: (value) => value == null ? 'Select Gender' : null,
-          onChanged: (value) {
-            setState(() {
-              _loanDistribution = value;
-            });
-          },
-          items: _loanDistributions.map((distribution) {
-            return DropdownMenuItem(
-              value: distribution['id'],
-              child: Text(_currency +
-                  " " +
-                  _format_currency(
-                      double.tryParse(distribution['max_amount']))),
-            );
-          }).toList(),
-          dropdownColor: HexColor('#000000'),
-          style: TextStyle(
-            fontWeight: FontWeight.w700,
-            color: Colors.white,
-            fontSize: 25.0,
-          ),
-          icon: Icon(Icons.keyboard_arrow_down),
-          iconSize: 33,
-          iconEnabledColor: Colors.white,
-          iconDisabledColor: Colors.white,
-        ),
-        // child: DropdownButton(
-        //   dropdownColor: HexColor('#000000'),
-        //   hint: Text("Select Gender"),
-        //   style: TextStyle(
-        //     fontWeight: FontWeight.w700,
-        //     color: Colors.white,
-        //     fontSize: 30.0,
-        //   ),
-        //   value: _loanDistributions,
-        //   icon: Icon(Icons.keyboard_arrow_down),
-        //   iconSize: 38,
-        //   iconEnabledColor: Colors.white,
-        //   iconDisabledColor: Colors.white,
-        //   items: _loanDistributions.map((distribution) {
-        //     return DropdownMenuItem(
-        //         value: distribution['id'],
-        //         child: Text(
-        //           distribution['max_amount'],
-        //           style: TextStyle(
-        //             fontWeight: FontWeight.w700,
-        //             color: Colors.white,
-        //             fontSize: 30.0,
-        //           ),
-        //         ));
-        //   }).toList(),
-        //   onChanged: (value) {
-        //     setState(() {
-        //       _loanDistribution = value;
-        //     });
-        //   },
-        // ),
+        child: _initDataFetched
+            ? DropdownButton(
+                value: _loanDistribution,
+                isExpanded: false,
+                hint: Text("Select Loan"),
+                // validator: (value) => value == null ? 'Select Gender' : null,
+                onChanged: (value) {
+                  setState(() {
+                    _loanDistribution = value;
+                    _calculateLoan(context);
+                  });
+                },
+                items: _loanDistributions.map((distribution) {
+                  return DropdownMenuItem(
+                    value: distribution['id'],
+                    child: Text(_currency +
+                        " " +
+                        _formatCurrency(
+                            double.tryParse(distribution['max_amount']))),
+                  );
+                }).toList(),
+                dropdownColor: HexColor('#000000'),
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  color: Colors.white,
+                  fontSize: 25.0,
+                ),
+                icon: Icon(Icons.keyboard_arrow_down),
+                iconSize: 33,
+                iconEnabledColor: Colors.white,
+                iconDisabledColor: Colors.white,
+              )
+            : Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                  ),
+                  height: 25.0,
+                  width: 25.0,
+                ),
+              ),
       ),
       color: HexColor('#4A1F1F'),
     );
@@ -162,7 +203,7 @@ class _DashboardState extends State<DashboardPage> {
               child: Padding(
                 padding: const EdgeInsets.all(13.0),
                 child: Text(
-                  "$_loanpaymentDays Days",
+                  _initDataFetched ? "$_loanpaymentDays Days" : "Loading...",
                   style: TextStyle(
                     fontWeight: FontWeight.w700,
                     color: HexColor('#4A1F1F'),
@@ -235,7 +276,7 @@ class _DashboardState extends State<DashboardPage> {
                       color: Colors.white,
                       size: 24.0,
                     ),
-                    onPressed: () => _refreshScreen(),
+                    onPressed: () => _refreshScreen(context),
                     style: ElevatedButton.styleFrom(primary: Colors.orange),
                   )
                 ],
@@ -250,7 +291,7 @@ class _DashboardState extends State<DashboardPage> {
                       style: TextStyle(fontSize: 18.0),
                     ),
                     Text(
-                      "Ksh 700",
+                      "$_currency $_loanDisbursed",
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         color: Colors.black,
@@ -270,7 +311,7 @@ class _DashboardState extends State<DashboardPage> {
                       style: TextStyle(fontSize: 18.0),
                     ),
                     Text(
-                      "Ksh 1,000",
+                      "$_currency $_totalLoanAmount",
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         color: Colors.black,
@@ -290,7 +331,7 @@ class _DashboardState extends State<DashboardPage> {
                       style: TextStyle(fontSize: 18.0),
                     ),
                     Text(
-                      "Ksh 270",
+                      "$_currency $_intrest",
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         color: Colors.black,
@@ -310,7 +351,7 @@ class _DashboardState extends State<DashboardPage> {
                       style: TextStyle(fontSize: 18.0),
                     ),
                     Text(
-                      "Ksh 30",
+                      "$_currency $_commission",
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         color: Colors.black,
@@ -369,7 +410,7 @@ class _DashboardState extends State<DashboardPage> {
                       style: TextStyle(fontSize: 18.0),
                     ),
                     Text(
-                      "07-January-2022",
+                      "$_applicationDateFormatted",
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         color: Colors.black,
@@ -389,7 +430,7 @@ class _DashboardState extends State<DashboardPage> {
                       style: TextStyle(fontSize: 18.0),
                     ),
                     Text(
-                      "14-January-2022",
+                      "$_dueDateFormatted",
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         color: Colors.black,
@@ -409,7 +450,7 @@ class _DashboardState extends State<DashboardPage> {
                       style: TextStyle(fontSize: 18.0),
                     ),
                     Text(
-                      "Ksh 1,000",
+                      "$_currency $_totalLoanAmount",
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         color: Colors.black,
@@ -433,27 +474,24 @@ class _DashboardState extends State<DashboardPage> {
       child: Padding(
         padding: const EdgeInsets.only(left: 10.0, right: 10.0),
         child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            primary: HexColor('#4A1F1F'), // background
-            onPrimary: HexColor('#4A1F1A'),
-            shape: StadiumBorder(), // foreground
-          ),
-          child: Padding(
-            padding: EdgeInsets.fromLTRB(40, 10, 40, 10),
-            child: Text(
-              'Apply Loan'.toUpperCase(),
-              style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white),
+            style: ElevatedButton.styleFrom(
+              primary: HexColor('#4A1F1F'), // background
+              onPrimary: HexColor('#4A1F1A'),
+              shape: StadiumBorder(), // foreground
             ),
-          ),
-          onPressed: () {
-            //After successful login we will redirect to profile page. Let's create profile page now
-            Navigator.pushReplacement(context,
-                MaterialPageRoute(builder: (context) => LoanApprovedPage()));
-          },
-        ),
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(40, 10, 40, 10),
+              child: Text(
+                _initDataFetched
+                    ? 'Apply Loan'.toUpperCase()
+                    : 'Loading...please wait',
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+            ),
+            onPressed: () => _applyLoan(context)),
       ),
     );
   }
